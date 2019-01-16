a989be5123eaaef7cee963b6ed03dc784e62b6d5
mmorales <mmorales@geocom.com.uy>
2019-01-16 10:50:08 -0300

    Se modifica sql de creación de tabla para que las fechas sean timestamp.

--- sfarma-server/sql/app-1.0.0.sql
+++ sfarma-server/sql/app-1.0.0.sql
@@ -6,14 +6,14 @@ CREATE TABLE `references_codes` (
   `source_description` varchar(255) DEFAULT NULL,
   `final_code` varchar(255) DEFAULT NULL,
   `final_presentation` varchar(255) DEFAULT NULL,
-  `updated` DATE DEFAULT CURRENT_TIMESTAMP,
+  `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   primary key (`source_code`, `source_barcode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE `references_locals` (
+ CREATE TABLE `references_locals` (
   `source_local_id` varchar(100) DEFAULT NULL,
   `final_local_id` varchar(100) DEFAULT NULL,
-  `updated` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   primary key (`source_local_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
